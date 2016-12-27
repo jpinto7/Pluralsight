c0c07c49da9d2a4de99dd8c3f5d8ad7c801df5c3
jpinto7 <juan.pinto108@gmail.com>
2016-12-27 13:21:24 -0600

    Added initial files for the starter app

--- Building Applications with React and Redux in ES6/starter/src/index.js
+++ Building Applications with React and Redux in ES6/starter/src/index.js
@@ -1,12 +1,9 @@
+import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
-  return (
-    <div>
-      Hola
-    </div>
-  );
-};
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
