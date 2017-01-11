e36cc5c8fcfc293f1541103f20175ddf82748937
Juan Pablo <Juan Pablo>
2017-01-11 17:59:41 -0600

    Added first thunk and courses table

--- Building Applications with React and Redux in ES6/starter/src/actions/courseActions.js
+++ Building Applications with React and Redux in ES6/starter/src/actions/courseActions.js
@@ -1,3 +1,18 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export const createCourse = course => ({type: types.CREATE_COURSE, course});
+export const createCourse = course =>
+  ({type: types.CREATE_COURSE, course});
+
+export const loadCoursesSuccess = courses =>
+  ({type: types.LOAD_COURSES_SUCCESS, courses});
+
+export const loadCourses = () => {
+  return dispatch => {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+};
--- Building Applications with React and Redux in ES6/starter/src/index.js
+++ Building Applications with React and Redux in ES6/starter/src/index.js
@@ -5,10 +5,14 @@ import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+
+store.dispatch(loadCourses());
+
 render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
