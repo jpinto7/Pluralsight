1b4f67de0f3345f946d7994f020d24e9ba071004
Juan Pablo <Juan Pablo>
2017-02-07 17:50:12 -0600

    Added sagas to fetch topics in navigation container

--- Building Scalable React Apps/starter/app/containers/NavigationContainer/index.js
+++ Building Scalable React Apps/starter/app/containers/NavigationContainer/index.js
@@ -6,10 +6,20 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import selectNavigationContainer from './selectors';
 import Navigation from '../../components/Navigation';
+import { requestTopics } from './actions';
 
 export class NavigationContainer extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    actions: React.PropTypes.object.isRequired
+  }
+
+  componentWillMount() {
+    this.props.actions.requestTopics();
+  }
+
   render() {
     return (
       <Navigation {...this.props} />
@@ -21,7 +31,9 @@ const mapStateToProps = selectNavigationContainer();
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    actions: {
+      requestTopics: () => dispatch(requestTopics())
+    }
   };
 }
 
--- Building Scalable React Apps/starter/app/containers/NavigationContainer/reducer.js
+++ Building Scalable React Apps/starter/app/containers/NavigationContainer/reducer.js
@@ -6,30 +6,21 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
+  REQUEST_TOPICS,
+  REQUEST_TOPICS_SUCCEEDED,
+  REQUEST_TOPICS_FAILED
 } from './constants';
 
 const initialState = fromJS({
-  topics: [
-    {
-      name: 'libraries',
-      description: 'links to useful open source libraries'
-    },
-    {
-      name: 'apps',
-      description: 'links to new and exciting apps'
-    },
-    {
-      name: 'news',
-      description: 'links to programming related new articles'
-    }
-  ]
+  topics: []
 });
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case REQUEST_TOPICS_SUCCEEDED:
+      return state.set('topics', action.topics);
+    case REQUEST_TOPICS_FAILED:
+      return state.set('message', action.message);
     default:
       return state;
   }
